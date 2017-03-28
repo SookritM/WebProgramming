5999f8fada23f0a3b7d09ccaef1e316b17500c48
Ming Chow <mchow@cs.tufts.edu>
2017-03-28 13:44:46 -0400

    Updated simpleexpress app

--- examples/nodejs/server.js
+++ examples/nodejs/server.js
@@ -5,9 +5,15 @@ http.createServer(function(request, response) {
 	
 	var pathname = url.parse(request.url).pathname;
 	console.log("Request for " + pathname + " received.");
+
+	// Rips out HTTP GET query string
 	var query = url.parse(request.url).query;
 	console.log("Query " + query + " received.");
+
+	// Sends HTTP status 200
 	response.writeHead(200, {"Content-Type": "text/plain"});
+
+	// Handle certain path requests
 	if (pathname == "/pikachu") {
 		response.write("You've won the game!");
 	}
