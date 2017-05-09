3aa00148b59bee5a191119fd6e61f47de874fd15
Ming Chow <mchow@cs.tufts.edu>
2017-05-09 18:26:03 -0400

    Cleaned up examples folder

--- examples/dom/helloannie.js
+++
@@ -1,6 +0,0 @@
-function highlightParagraphs() {
-	paragraphs = document.getElementsByTagName("p");
-	for (i = 0; i < paragraphs.length; i++) {
-		paragraphs[i].style.backgroundColor = "yellow";
-	}
-}
